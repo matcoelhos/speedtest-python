8b1891dc6321da0ec11a0409df3b0aa75af4efc7
Mateus Coelho <matcoelhosilva@yahoo.com.br>
2021-05-06 19:13:17 -0300

    Update test.py
    Addition of timing constrainers to increase stability

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import speedtest
 import datetime
+from time import sleep
 
 servers = []
 # If you want to test against a specific server
@@ -10,18 +11,22 @@ threads = None
 # threads = 1
 
 s = speedtest.Speedtest()
-#s.get_servers(servers)
+s.get_servers(servers)
 
 fid = open('data.txt','w')
+
+print('Starting Test')
 for i in range(100):
 	try:
 		s.get_best_server()
+		sleep(5) # time for settling the best server
 		d = float(s.download(threads=threads))/1000000
 		u = float(s.upload(pre_allocate=False,threads=threads))/1000000
 		ct = datetime.datetime.now()
 		p = s.results.ping
 		print("%.2f\t%.2f\t%.2f\t%s"%(d,u,p,ct))
 		fid.write("%.2f\t%.2f\t%.2f\t%s"%(d,u,p,ct))
+		sleep(10) # wait interval
 	except KeyboardInterrupt:
 		break
 
@@ -31,4 +36,3 @@ fid.close()
 #s.results.share()
 
 results_dict = s.results.dict()
-
